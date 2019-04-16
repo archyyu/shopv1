0de2b7ad7e82a34164d31420fbd80c2cbdfbd36c
archy_yu <archy_yu@163.com>
2019-04-16 22:38:00 +0800

    添加商品规格

---
+++ addons/shopv1/core/model/ShopProductUnit.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace model;
+
+/**
+ * Description of ShopProductUnit
+ *
+ * @author YJP
+ */
+class ShopProductUnit extends Model{
+    
+    protected $table = "shopv1_product_unit";
+    
+    
+}
