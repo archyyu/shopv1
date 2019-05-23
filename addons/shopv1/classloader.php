901e82628a703bdd5967e53b9515685fc3284cdd
archy_yu <archy_yu@163.com>
2019-05-23 13:57:29 +0800

    Update classloader.php

--- addons/shopv1/classloader.php
+++ addons/shopv1/classloader.php
@@ -7,70 +7,9 @@
  */
 
 
-class ClassLoader{
-    
-    private static $includedList = array();
-    
-    public static function recurseInclude($dir){
 
-        if(is_file($dir)){
-            include($dir);
-            ClassLoader::$includedList[$dir] = true;
-        }
-        else{
-            $files = file_tree($dir);
-            foreach($files as $file){
-                if(is_dir($file)){
-                    ClassLoader::recurseInclude($file);
-                    continue;
-                }
-                else{
-                    if(isset(ClassLoader::$includedList[$file]) == false){
-                        include($file);
-                        ClassLoader::$includedList[$file] = true;
-                    }
-                }
-            }
-        }
-    }
-    
-    public static function recurseFindByClassName($dir,$className){
-        $files = file_tree($dir);
-        foreach($files as $file){
-            if(is_dir($file)){
-                ClassLoader::recurseFindByClassName($file);
-            }
-            else{
-                if(basename($file) == $className.".php"){
-                    return $file;
-                }
-            }
-        }
-    }
- 
-    public static function includeFile($file){
-        if(isset(ClassLoader::$includedList[$file]) == false){
-            include($file);
-            ClassLoader::$includedList[$file] = true;
-        }
-    }
- 
-    public static function includeBaseClass($dir,$className){
-        $file = ClassLoader::recurseFindByClassName($dir, $className);
-        if(isset($file)){
-            if(isset(ClassLoader::$includedList[$file]) == false){
-                include($file);
-                ClassLoader::$includedList[$file] = true;
-            }
-        }
-    }
-    
+function autoLoad($className){
+    include (CASHROOT."core/".$className.".php");
 }
 
-
-ClassLoader::includeFile(CASHROOT."core/model/Medoo.php");
-ClassLoader::includeFile(CASHROOT."core/service/Service.php");
-ClassLoader::includeFile(CASHROOT."core/controller/Controller.php");
-
-ClassLoader::recurseInclude(CASHROOT."core");
-
+spl_autoload_register('autoLoad');
